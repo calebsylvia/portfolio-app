4d92b149923f435df41367bbb94ec747756c34c8
Caleb Sylvia <caleb_sylvia@yahoo.com>
2024-05-13 13:47:21 -0700

    fix headshot

--- src/app/Components/Headshot.tsx
+++ src/app/Components/Headshot.tsx
@@ -6,7 +6,7 @@ const Headshot = () => {
   return (
     <>
     <div className="md:pt-20">
-        <div className="animate-[spin_5s_linear_infinite] bg-gradient-to-r from-[#495464] from-50% to-[rgb(244,244,242)] p-0  md:p-28 lg:p-48 rounded-[250px] md:rounded-[50%] relative"></div>
+        <div className="animate-[spin_5s_linear_infinite] bg-gradient-to-r from-[#495464] from-50% to-[rgb(244,244,242)] p-0  md:p-36 lg:p-48 rounded-[250px] md:rounded-[50%] relative"></div>
         <Image src={caleb} alt="Caleb Sylvia Headshot" className="w-56 md:w-72 lg:w-96 rounded-[50%] p-2 absolute top-[425px] md:top-[175px]"/>
     </div>
     </>
